58ac634efb00e483f02f97f8899e5dfd78926b0f
JoleneMel <jolenemelanson33@gmail.com>
2022-11-07 13:19:13 -0500

    first commit

--- src/Week2Vids/BooleanOps.java
+++ src/Week2Vids/BooleanOps.java
@@ -3,30 +3,46 @@ package Week2Vids;
 public class BooleanOps {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		/*
-		 * boolean data are data that can either be true or false some examples of boolean data in our
-everyday life include answers to yes-or-no questions such as is it hot outside is the dinner finished are we
-there yet the answers to these questions are either yes or no similar to boolean values of true or false the answers to
-any of these questions or any question like it is typically used to determine a further action for example if it is hot outside
-then we won't wear a snow coat if dinner is finished we will wash our hands and
-get on to the table and if the answer - are we there yet is no than our children will entertain themselves for a few more
-minutes and ask again in order to identify and evaluate boolean data in our code we can use boolean operators to
-check things such as if a value is equal to another value whether a value is greater than or less than another value
-or if multiple conditions are true together any of these comparisons will result in true or false and that is
-called boolean logic to perform boolean logic we use boolean operators and when we combine boolean values and at least
-one boolean operator we get a boolean expression and that expression will evaluate to another boolean value either
-true or false some of the operators that we can work with are the Equality operator to check to see if two values
-are equal we have the greater than and less than operators to see if something is greater than or less than another
-value we have the logical and and or operators which allow us to combine multiple boolean expressions such as if
-we're looking for two conditions like I want this and this or I want this or that and then when we put these all
-together we can build complex boolean logic that allow our programs to make decisions so the reason that boolean
-values are important is they help us make decisions right programming is all about looking at data manipulating that
-data and doing something with it well that doing something with it part is all about making a decision without having
-different logical paths we go if yes go this way if no go that way our programs are very useful so let's
-take a look at some of the boolean expressions that we can have starting with the Equality operator
-		 */
-		System.out.println("hello");
+		Using programming to automate tasks means the computer needs a way to make decisions. Decisions require comparing and evaluating information available and then deciding which way to proceed. Boolean values are essential in these comparisons. With each decision we need the computer to make, we eventually answer in a yes or no manner - in Java, that yes or no is represented by:  true or false 
+
+
+		Boolean Expression -- Legally Old Enough To Drive Example:
+
+
+		Imagine that we want to know if a person is old enough to drive.  To determine that,  we need to compare the individual's age:   currentAge     to the age required to drive:   ageRequiredToDrive  
+
+		If the person's currentAge is great than or equal to the ageRequiredToDrive  then the person can drive.
+		If not, the person cannot legally drive. true and false are the only two options. 
+
+		Here is an example of how this could be represented in Java: */
+
+		int ageRequiredToDrive = 16;
+
+		int currentAge = 14;
+
+		boolean canPersonDrive = (currentAge >= ageRequiredToDrive);
+
+		System.out.println(canPersonDrive);
+/*
+		Some Observations:
+
+		    The code above will compare our two variables:   ageRequiredToDrive and currentAge 
+		    We use an if statement to determine if currentAge is greater than or equal to ageRequiredToDrive
+		        If the value stored currentAge is greater than or equal to agetRequiredToDrive the result of the Boolean expression (the operation using the >= operator) based on the values assigned to those variables would evaluate to true 
+		        If the value stored in currentAge was less than ageRequiredToDrive, then the result would be false. 
+		    The result of the Boolean expression is assigned to the variable canPersonDrive and then printed to the console. In this case, the Boolean expression evaluates to false. 
+		    Note:  If currentAge were 16, 17, or another higher number, it would evaluate to true
+
+
+		List of Boolean operators that exist:
+
+		    Less than:   <
+		    Greater than:   >
+		    Less than or equal to:   <=
+		    Greater than or equal to:   >=
+		    Equal (type matters):   ==
+		    */
 
 	}
 
