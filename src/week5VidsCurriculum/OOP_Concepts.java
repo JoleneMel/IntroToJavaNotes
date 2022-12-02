b53085e24cc43f59bff48d691672c7c78322c6f5
JoleneMel <jolenemelanson33@gmail.com>
2022-12-02 13:22:24 -0500

    adding week 5 notes

---
+++ src/week5VidsCurriculum/OOP_Concepts.java
@@ -0,0 +1,70 @@
+package week5VidsCurriculum;
+
+public class OOP_Concepts {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		What is OOP?
+
+
+				Object Oriented Programming (OOP) is a programming paradigm.  Java is an Object Oriented Language, which means that it supports OOP, providing features that 
+				support and implement the Four Pillars of OOP. 
+
+				In OOP,  objects are the key construct in our programming, and these objects contain properties and functionality.  Instead of writing code all in one place, 
+				we structure our program into objects.  Each object will contain relevant properties and functionality, which is code that is structured into methods that 
+				can be used over and over.  
+
+				 In OOP, programs are written by creating objects that contain both data (properties)  and methods (functionality),  in contrast with procedural programming 
+				where data exists, and  procedures and methods perform operations on the data.
+
+				It takes time to start to think of data as being stored in an object which also contains all of the methods written for that object.  Another way to explain 
+				this is to think of an object, and imagine that everything that you need (data or methods) is accessible anytime you reference that object.  If you have 
+				access to the object, you also have access to any available property and functionality that belong to that object.
+
+
+				Terminology -- Classes vs. Objects:
+
+
+				Classes are the mechanism by which we structure our code in OOP.
+
+				    A Class is a blueprint, in which you create your properties and functionality.
+				    An Object is the actual product developed from the blueprint.
+
+				An Object is an instance of a Class!
+
+
+				So, a class is a template for objects, and an object is an instance of a class.  When we create an object from a class, that object will inherit everything in 
+				the class, including the properties and functionality.
+
+
+				Description of a Class and an Object created from the Class:
+
+				Class:  Animal
+
+				Properties:
+
+				    name
+				    weight
+				    type
+				    location
+
+				Functionality:
+
+				    describe()
+
+				Object:  dog 
+
+				A dog will be instantiated as an instance of the class, Animal.
+
+				 Animal dog = new Animal();
+
+				Preview:
+
+				In the next chapter, 2. Classes, you will find a Java example that creates a class Animal, and creates a class AnimalMain, in which we have a main method, as 
+				well as two objects created from the class Animal:  elephant and kangaroo. 
+				*/
+		
+	}
+
+}
