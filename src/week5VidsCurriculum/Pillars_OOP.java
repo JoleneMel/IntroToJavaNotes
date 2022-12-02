54454cfa4bed602ffc69c98656cdad9e855887ee
JoleneMel <jolenemelanson33@gmail.com>
2022-12-02 13:43:36 -0500

    adding week 5 notes

---
+++ src/week5VidsCurriculum/Pillars_OOP.java
@@ -0,0 +1,79 @@
+package week5VidsCurriculum;
+
+public class Pillars_OOP {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		Object-Oriented Programming is an Imperative Programming Paradigm.  
+
+		Let's step back a little bit, and give you some background on programming paradigms.
+
+		Definitions:  
+
+		    A paradigm is a way, or a method that can be used to solve a problem. 
+		    A programming paradigm is the way that one solves a given problem (or task), by programming.  Also described as a framework that defines how problems can be solved.
+		    A programming language (e.g. Java, Pascal, C#, etc.) is separate from a programming paradigm. A programming language is a tool that is used to write code to solve problems.
+
+
+		There are a number of programming paradigms.  Each one has a set of rules and principles.  If a programming language is going to support a programming paradigm,  the programming 
+		language must support or adhere to those rules and principles    The programming paradigm is enforced by a programming language compiler during the program compilation stage.  
+		Each programming paradigm is simply a different approach to solve a problems, and provides a style and organization to do so.  
+
+		Here is a brief overview of a few Programming Paradigms:
+
+		Imperative:  These types of paradigms are command driven. 
+		Details:  Support of an imperative programming paradigm looks like this:
+
+		        The program code directs the program execution as sequence of statements executed one by one.
+		        The statements will direct the computer to perform a tasks.
+		        The programmer designs and programs each statement in detail, and each of those statements will direct what is to be accomplished, by specifying how it is to be done.
+		        A program, in an imperative programming paradigm, is simply a set of statements, run in order, that execute to completion.
+
+		Types:  Two common imperative programming paradigms are as follow:
+
+		        Procedural -- Based on a set of procedures (also referred to as functions, methods or subroutines).  Each procedure, function, method or subroutine performs a 
+		        specific operation.  C, FORTRAN, and Pascal support this paradigm.
+
+		        Object-Oriented --  All program components are objects.   The unique feature here is that an object contains properties and functions.  All data and associated 
+		        methods are combined into a single object.  Some would say that this is an extension of procedural programming concepts.   Java supports this paradigm.
+
+
+		Declarative:  These types of paradigms are logic focused.
+		Details: Support of a declarative programming paradigm looks like this:
+
+		        The control flow is not the important part of this program, the logic of a computation is expressed, without describing the control flow of the computation.
+		        Focused on achieving the end result.
+		        This approach describes to what is to be done, instead of how it should be achieved.
+
+		Types:
+
+		        Functional -- based on mathematical functions.  Attempts to solve problems with composing functions as program components.  functions can be passed as 
+		        arguments, and can also be returned as a value.  Pure functional languages, (e.g. Haskell) allow only pure functional programs.  However, some languages 
+		        allow both functional and procedural programs, and Java is one of these programming languages, though to effectively use functional programming in Java, 
+		        it is advisable to use Java 8 or later.
+
+		        Logic -- based on logic and control, or in other words facts and rules.  Control is defined as an order of rules.  In this type, logic defines what needs 
+		        to be solved whereas the control defines how it should be solved.  Prolog is an example of this paradigm.  
+
+
+		Note:  many of the current programming languages are general purpose languages, and as such, they support more than one programming paradigm.
+
+		Our focus here is on Object-Oriented Programming (OOP).  There are a number of programming languages that support the object oriented programming paradigm, 
+		including Java, JavaScript, C3, Ruby, and others.   
+
+		There are four pillars of OOP which are:
+
+		    Encapsulation -- Data Security -- binding related data and methods together into objects -- data hiding (hide the unnecessary).
+		    Inheritance -- Code reusability -- acquiring the existing functionality of a parent class, with the ability to add additional functionality and features 
+		    into the child class -- an object can inherit some properties and methods from another object.
+		    Polymorphism --  Many Forms --  A single object can have multiple behaviors, or respond in different ways to the same function.  
+		    Abstraction -- Present a simplified view -- Hide the complexity from the user (show only what is necessary).
+
+		Each pillar is essential, and important, but these pillars are also dependent upon each other.  Without encapsulation, abstraction and inheritance are not possible. 
+		Additionally, polymorphism does not exist without inheritance.   Each of these pillars is essential to the paradigm, and together define object-oriented programming.   
+		OOP as a whole functions through objects the contain properties and methods, and the relationships of these objects with other objects.
+		*/
+	}
+
+}
